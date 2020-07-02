813e298b266c61aee8ad96424d4a7e419f0382c0
vinanavy <vinanavy@gmail.com>
2020-07-02 17:37:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, NavLink} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {apiResponse:""};
+	}
+
+	callAPI() {
+    fetch("http://localhost:8080/index")
+		  .then(res => res.text())
+		  .then(res => this.setState({apiResponse: res}));
+	}
+
+	componentWillMount() {
+		this.callAPI();
+	}
+
+	render() {
+		return (
+			<div className="App">
+        <BrowserRouter>
+          <div>
+            <div className="header">
+              <NavLink exact activeClassName="active" to="/">Home</NavLink>
+              <NavLink exact activeClassName="active" to="/login">Home</NavLink>
+            </div>
+          </div>
+        </BrowserRouter>
+			</div>
+		);
+	};
 }
 
 export default App;
